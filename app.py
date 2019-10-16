c179bfcf185cb03231a90a5b51d1f5776520f0ae
AM <anvar.minulin@gmail.com>
2019-10-16 22:05:29 +0300

    Modify token generation method to POST

--- app.py
+++ app.py
@@ -5,12 +5,23 @@ from PhonebookModel import *
 from test import valid_phonebook_entity
 import jwt
 import datetime
+from UserModel import User
 
-@app.route('/login')
+@app.route('/login', methods = ['POST'])
 def get_token():
-    expiration_date = datetime.datetime.utcnow() + datetime.timedelta(seconds=100)
-    token = jwt.encode({'exp' : expiration_date}, app.config['SECRET_KEY'], algorithm = 'HS256')
-    return token
+    request_data = request.get_json()
+    username = str(request_data['username'])
+    password = str(request_data['password'])
+    match = User.username_password_match(username, password)
+
+    if match:
+        expiration_date = datetime.datetime.utcnow() + datetime.timedelta(seconds=100)
+        token = jwt.encode({'exp': expiration_date}, app.config['SECRET_KEY'], algorithm='HS256')
+        return token
+
+    else:
+        return Response("", 401, mimetype = 'application/json')
+
 
 #GET /phonebook
 @app.route('/phonebook')
