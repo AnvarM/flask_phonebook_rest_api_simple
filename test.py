1b44966a90168f6a5a7789b9258d5000ab968d67
AM <anvar.minulin@gmail.com>
2019-11-23 21:48:34 +0300

    Fix tests

--- test.py
+++ test.py
@@ -71,7 +71,7 @@ missing_city = {"name" : "Guta",
 
 assert valid_phonebook_entity(valid_entity) == True, "Should be True"
 assert valid_phonebook_entity(missing_name) == False, "Should be False"
-assert valid_phonebook_entity(missing_last_name) == False, "Should be False"
+assert valid_phonebook_entity(missing_last_name) == True, "Should be True"
 assert valid_phonebook_entity(missing_email) == True, "Should be True"
 assert valid_phonebook_entity(missing_birthday) == True, "Should be True"
 assert valid_phonebook_entity(missing_birthday) == True, "Should be True"
