cd4b667d55601e416890054e935c48b4a138ea79
AM <anvar.minulin@gmail.com>
2019-11-30 21:09:57 +0300

    Fix tests

--- test.py
+++ test.py
@@ -80,4 +80,6 @@ assert valid_phonebook_entity(missing_birthday) == True, "Should be True"
 assert valid_phonebook_entity(missing_country) == True, "Should be True"
 assert valid_phonebook_entity(missing_city) == True, "Should be True"
 assert valid_phonebook_entity(missing_city) == True, "Should be True"
+assert valid_phonebook_entity(missing_city) == True, "Should be True"
+assert valid_phonebook_entity(missing_city) == True, "Should be True"
 
