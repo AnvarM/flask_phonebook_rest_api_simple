87327290967d2dd8f4adc196e6f068dd04aae24b
AM <anvar.minulin@gmail.com>
2019-12-02 20:03:18 +0300

    Fix tests

--- test.py
+++ test.py
@@ -81,5 +81,6 @@ assert valid_phonebook_entity(missing_country) == True, "Should be True"
 assert valid_phonebook_entity(missing_city) == True, "Should be True"
 assert valid_phonebook_entity(missing_city) == True, "Should be True"
 assert valid_phonebook_entity(missing_city) == True, "Should be True"
+assert valid_phonebook_entity(missing_city) == True, "Should be True"
 
 
