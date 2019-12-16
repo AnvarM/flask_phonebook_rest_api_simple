cfd5d373993ef749e557fbcbb03b8315ad163ad1
AM <anvar.minulin@gmail.com>
2019-12-16 22:07:38 +0300

    Fix tests

--- test.py
+++ test.py
@@ -78,6 +78,7 @@ assert valid_phonebook_entity(missing_email) == True, "Should be True"
 assert valid_phonebook_entity(missing_birthday) == True, "Should be True"
 assert valid_phonebook_entity(missing_country) == True, "Should be True"
 assert valid_phonebook_entity(missing_city) == True, "Should be True"
+assert valid_phonebook_entity(missing_city) == True, "Should be True"
 
 
 
